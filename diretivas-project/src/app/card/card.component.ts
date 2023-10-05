60fd7607cd4a53f346a4cfc288121eec138d7345
Rafael Dametto <cacotpds@gmail.com>
2023-10-05 16:10:21 -0300

    Switch e Switch Case

--- diretivas-project/src/app/card/card.component.ts
+++ diretivas-project/src/app/card/card.component.ts
@@ -7,15 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
   produtos: string[] = [];
+  menuTipe: string = '';
   constructor() {
     this.produtos = ['Mouse', 'Teclado', 'Webcan', 'Monitor', 'Gabinete'];
   }
 
   ngOnInit(): void {}
-
+  // função que adiciona reação através do add() + *ngFor="let p of produtos no html
   add() {
     this.produtos.push('Placa de video');
   }
+  //função que rastreia a lista através do remove() + let i = index no html
   remover(index: number) {
     this.produtos.splice(index, 1);
   }
